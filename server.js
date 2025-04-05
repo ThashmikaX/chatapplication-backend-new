fdd1d5309b4400fcb64220089e8567f7f9cbdcd2
ThashmikaX <thashmikax2gmail.com>
2025-04-05 21:29:14 +0530

    change default server port from 8080 to 5000

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ app.post('/api/users', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 }); 
